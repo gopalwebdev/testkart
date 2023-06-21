ae8b4e24284fbff32a56b093efc5653346a165d7
Gopal <gopal.n@theswipewire.in>
2023-06-21 23:25:07 +0530

    fix vite config

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+
+
     <title>@yield('title')</title>
     <style>
         body {
